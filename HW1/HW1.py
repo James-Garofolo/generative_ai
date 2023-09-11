6345e525befbfe3332614d2e0360428935c91cb0
James-Garofolo <garofo24@students.rowan.edu>
2023-09-11 15:43:54 -0400

    finished homework 1

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -17,6 +19,10 @@ print(f"Pre-shaped training data: {xtrain.shape}")
 print(f"Pre-shaped testing data: {xtest.shape}")
 print(f"Flattened training data: {xtrain.reshape([xtrain.shape[0], -1]).shape}")
 print(f"Flattened testing data: {xtest.reshape([xtest.shape[0], -1]).shape}")
+print(f"Pre-normalized training max: {tf.math.reduce_max(xtrain)}, min: {tf.math.reduce_min(xtrain)}")
+print(f"Pre-normalized testing max: {tf.math.reduce_max(xtest)}, min: {tf.math.reduce_min(xtest)}")
+print(f"Normalized training max: {tf.math.reduce_max(xtrain/255)}, min: {tf.math.reduce_min(xtrain/255)}")
+print(f"Normalized testing max: {tf.math.reduce_max(xtest/255)}, min: {tf.math.reduce_min(xtest/255)}")
 
 plt.subplots_adjust(hspace=1.0, wspace=0.5)
 plt.show()
